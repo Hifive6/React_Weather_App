2cf6f36c0aa7865c9856c0fc0f3cb5fc88719cc7
David Juarez <djuar05@gmail.com>
2020-04-23 14:28:25 -0400

    rendered to page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React, {Component} from 'react';
-
 import './App.css';
-const APIKEY = '2c86b8e9b118e71a75326c043e5ec819'
-
-
 
 class App extends Component {
   
-    state = {
-      zipcode: '',
-    }
+    state = {};
 
     getWeather = (e) => {
       e.preventDefault();
       let zipcode = e.target.value
+      
       //  this.setState({[e.target.zipcode]: e.target.value})
       // console.log(zipcode)
       
       fetch(
-      'https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + APIKEY) 
+      'https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + process.env.REACT_APP_OW_API + '&units=imperial') 
       .then(
-        function(response) {
+        ((response) => {
           if (response.status !== 200) {
             console.log('Looks like there was a problem. Status Code: ' +
               response.status);
+            let errorMessage = "Please add a valid Zipcode"
+            
             return;
           }
-    
+        
           // Examine the text in the response
           response.json().then((data) => {
-            console.log(data.name);
-          });
-        }
+            console.log(data); 
+            // if(zipcode === 6){
+              console.log('valid zipcode')
+            // } else {
+            this.setState({
+                  temperature: data.main.temp,
+                  forecast: data.weather[0].main,
+                  feels_like: data.main.feels_like,
+                  name:data.name,
+                  
+            
+                })
+              // }
+          // this.getTime()
+          })
+        })
       )
+      
+    
       .catch(function(err) {
         console.log('Fetch Error :-S', err);
       });
-      // this.onChange = this.onChange.bind(this);
+    // this.onChange = this.onChange.bind(this);
     
   }
     
@@ -48,16 +60,22 @@ class App extends Component {
     
     <h1>Your Weather </h1>
     <div>
-      
       <input 
       type='text'
       value={this.state.value}
       onChange={this.getWeather} 
       placeholder='Enter Zipcode'>
       </input>
-      <button 
+      {/* <button 
       onClick={this.onClick}
-      >Search ZipCode</button>
+      >Search ZipCode</button> */}
+    </div>
+
+    <div>
+      <h2 id="name">{this.state.name}</h2>
+      <div id="temperature">{this.state.temperature}</div>
+      <div id="forcast">{this.state.feels_like}</div>
+      <div id="feels_like">{this.state.forecast}</div>
     </div>
     
     
